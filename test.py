80e29a3c472197d36c9973a1d220048b03565f49
rajatarun <traja1@binghamton.edu>
2015-11-10 18:32:05 -0500

    third commit

--- test.py
+++ test.py
@@ -1,4 +1,37 @@
 import urllib as a
-b = a.urlopen("https://s3-us-west-2.amazonaws.com/tarunproject/3students.txt")
+b = a.urlopen("https://s3-us-west-2.amazonaws.com/tarunproject/acne.txt");
 c = b.read();
-d = c.replace("\r"," ").replace("\n"," ").strip().split(".")
+c = c.replace("\r","").replace("\r","");
+import re
+d = re.findall("[a-zA-Z0-9!? ,'""*;:-]*[^.\\r\\n,]",c);
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf = TfidfVectorizer();
+tf = tfidf.fit_transform(d)
+mat = cosine_similarity(tf,tf)
+import numpy
+m = numpy.array(mat).tolist();
+sum = [];
+for i in m:
+    sum1 = 0;
+	for j in i:
+        sum1 = sum1+j;
+	sum.append(sum1);
+val = [];
+for i in range(len(sum)):
+	if(sum[i] >= round(numpy.median(sum))):
+		val.append(i);
+di = [];
+for i in val:
+	di.append(d[i]);
+
+
+
+
+	
+
+
+
+
+
+
+
